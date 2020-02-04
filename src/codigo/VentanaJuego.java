ed391b3f1aa9ea147faa04027bc27c672a7008af
mirenordonezdearce <mirenordonezdearce@10.10.11.157>
2020-02-04 20:11:07 +0100

    Con el temporizador iniciado.

--- src/codigo/VentanaJuego.java
+++ src/codigo/VentanaJuego.java
@@ -5,17 +5,68 @@
  */
 package codigo;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import javax.swing.Timer;
+
 /**
  *
  * @author jorgecisneros
  */
 public class VentanaJuego extends javax.swing.JFrame {
 
+    static int ANCHO_PANTALLA = 800;
+    static int ALTO_PANTALLA = 600;
+    
+    int filasMarcianos = 5;
+    int columnasMarcianos = 10;
+    
+    BufferedImage buffer = null;
+    
+    //Bucle de animación del juego. En este caso, es un hilo de ejecución nuevo que se 
+    //encarga de refrescar el contenido de la pantalla. 
+    Timer temporizador = new Timer(10, new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            //TODO: código de la animación. 
+            bucleDelJuego();
+        }
+    });
+    
     /**
      * Creates new form VentanaJuego
      */
     public VentanaJuego() {
         initComponents();
+        setSize(ANCHO_PANTALLA, ALTO_PANTALLA);
+        //Creamos una imagen del mismo alto y ancho que el jPanel1 y lo guarda en el buffer.
+        buffer = (BufferedImage) jPanel1.createImage(ANCHO_PANTALLA, ALTO_PANTALLA); 
+        buffer.createGraphics(); //Esta crea el contexto gráfico del buffer. 
+        
+        //Arrancamos el temporizador para que empiece el juego. 
+        temporizador.start();
+                
+    }
+    
+    //Creamos el método que va a ir en el hilo secundario. 
+    private void bucleDelJuego() {
+        //Este método gobierna el redibujado de los objetos en el jPanel. 
+        
+        //1º borro todo lo que hay en el buffer. 
+        Graphics2D g2 = (Graphics2D) buffer.getGraphics();
+        g2.setColor(Color.BLACK);
+        g2.fillRect(0, 0, ANCHO_PANTALLA, ALTO_PANTALLA);
+        
+        //////////////////////////////////////////////////
+        
+        //////////////////////////////////////////////////
+        
+        //2º dibujo de golpe todo el buffer sobre el jPanel. 
+        g2 = (Graphics2D) jPanel1.getGraphics();
+        g2.drawImage(buffer, 0, 0, null);
     }
 
     /**
@@ -27,17 +78,30 @@ public class VentanaJuego extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 453, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 430, Short.MAX_VALUE)
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
@@ -79,5 +143,6 @@ public class VentanaJuego extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
